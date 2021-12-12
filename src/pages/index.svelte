8ded2ca6f1c98d1f67568f1ef809d7abf227b966
Beatrice Kristine Becker <beakrisbec@gmail.com>
2021-12-12 15:15:36 +0100

    init

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -4,5 +4,8 @@
     metatags.title = 'Lemonade Palace'
     metatags.description = 'Description coming soon...'
 </script>
+<h1 class="cursor-pointer text-center text-orange-600 text-4xl pt-24">Willkommen im Limonadenpalast:</h1>
+<h1 class="text-center text-orange-600 text-4xl pt-26">Das lecker spritzige Erfrischungsgetränk</h1>
 
-<h1 class="cursor-pointer h-screen">Hallo Beatrice</h1>
+
+<h2>tschössssssssssssssss</h2>
