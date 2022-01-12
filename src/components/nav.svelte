9ee7122ab7a87b44cc10f04dc5d2c28342976631
BeaBkr <95778749+BeaBkr@users.noreply.github.com>
2022-01-12 21:59:16 +0100

    Add files via upload

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,6 +1,11 @@
-<nav class="flex flex-row justify-between">
-    <a href="#">BEAS LOGO</a>
-    <a href="#">Was</a>
-    <a href="#">soll hier</a>
-    <a href="#">stehen</a>
+<nav class="flex flex-row py-10 px-8 justify-between relative">
+    <a href="/shop" class="p-2 border-y-0 hover:border-y-2 hover:border-solid hover:border-black">s h o p</a>
+    <a href="/about" class="p-2 border-y-0 hover:border-y-2 hover:border-solid hover:border-black">a b o u t &nbsp; u s</a>
+    <a href="/" class="text-center">
+        <div class="font-balestic text-6xl">Teadrink</div>
+        <div>d r i n k s &nbsp; c o m p a n y</div>
+        <hr class="w-1/4 border-black">
+    </a>
+    <a href="/impressum" class="p-2 border-y-0 hover:border-y-2 hover:border-solid hover:border-black">i m p r e s s u m</a>
+    <a href="/information" class="p-2 border-y-0 hover:border-y-2 hover:border-solid hover:border-black">i n f o r m a t i o n</a>
 </nav>
