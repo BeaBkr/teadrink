4793ae741bb5eb407426e1b36ec34802b76ce79f
BeaBkr <95778749+BeaBkr@users.noreply.github.com>
2022-01-12 21:57:05 +0100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import "./global.css";
-import HMR from "@roxi/routify/hmr";
-import App from "./App.svelte";
-
-const app = HMR(App, { target: document.body }, "routify-app");
-
-export default app;
